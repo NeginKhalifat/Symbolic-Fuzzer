62f651938320c86ed74b42cc2650544e1e377ec5
Fateme Masoudi <fatemeh78msd@gmail.com>
2021-05-23 10:15:22 +0430

    add to_src , get_expression & prefix_vars function

--- src/HelperFunc.py
+++ src/HelperFunc.py
@@ -1,3 +1,14 @@
+import sys
+import inspect
+import z3
+import ast
+import astor
+from fuzzingbook.ControlFlow import PyCFG, CFGNode, to_graph, gen_cfg
+from graphviz import Source, Graph
+from fuzzingbook.Fuzzer import Fuzzer
+from contextlib import contextmanager
+
+
 def show_cfg(fn, **kwargs):
     return Source(to_graph(gen_cfg(inspect.getsource(fn)), **kwargs))
 
@@ -16,3 +27,37 @@ def get_symbolicparams(fn):
     return [SYM_VARS[typ][0](name)
             for name, typ in params], SYM_VARS[ret][0]('__return__')
 
+def to_src(astnode):
+    return astor.to_source(astnode).strip()
+
+
+def get_expression(src):
+    return ast.parse(src).body[0].value
+
+def prefix_vars(astnode, prefix):
+    if isinstance(astnode, ast.BoolOp):
+        return ast.BoolOp(astnode.op,
+                          [prefix_vars(i, prefix) for i in astnode.values], [])
+    elif isinstance(astnode, ast.BinOp):
+        return ast.BinOp(
+            prefix_vars(astnode.left, prefix), astnode.op,
+            prefix_vars(astnode.right, prefix))
+    elif isinstance(astnode, ast.UnaryOp):
+        return ast.UnaryOp(astnode.op, prefix_vars(astnode.operand, prefix))
+    elif isinstance(astnode, ast.Call):
+        return ast.Call(prefix_vars(astnode.func, prefix),
+                        [prefix_vars(i, prefix) for i in astnode.args],
+                        astnode.keywords)
+    elif isinstance(astnode, ast.Compare):
+        return ast.Compare(
+            prefix_vars(astnode.left, prefix), astnode.ops,
+            [prefix_vars(i, prefix) for i in astnode.comparators])
+    elif isinstance(astnode, ast.Name):
+        if astnode.id in {'And', 'Or', 'Not'}:
+            return ast.Name('z3.%s' % (astnode.id), astnode.ctx)
+        else:
+            return ast.Name('%s%s' % (prefix, astnode.id), astnode.ctx)
+    elif isinstance(astnode, ast.Return):
+        return ast.Return(prefix_vars(astnode.value, env))
+    else:
+        return astnode
