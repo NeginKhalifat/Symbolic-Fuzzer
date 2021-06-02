5b00e6e46167b0f83fd98eb1cb0cf6f26cee0184
m z <melikaaa98@yahoo.com>
2021-06-02 23:19:18 +0430

    change test2

--- Examples/reassignment.py
+++ Examples/reassignment.py
@@ -2,12 +2,12 @@ def fun(a: int,b: int,c: int):
     x: int = 0
     y: int = 0
     z: int = 0
-    if (a >= 0):
-        x = -2
-    
-    if (b < 5):
-        if (a <= 0 ):
-            if( c < 7):
+    if (a > 0):
+        b += -a
+
+    if (b > a):
+        if (a <= 0):
+            if (c < 7):
                 y = 1
         z = 2
     return x + y + z
